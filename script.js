d1f4717612027ea1a6fa0276ca1df971aff6da1a
robafuqara1989 <57313370+robafuqara1989@users.noreply.github.com>
2019-11-06 17:49:43 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+        var img = document.createElement("img);
+        img.src = "https://www.nardin.org/uploaded/PDFs/MontPhoto5_(2).jpg"
+        var src = document.getElementById("header");
+        src . appendchild(img);
+        var child = 10;
+        var msg;
+        if (child == 10) {
+            msg-
+        }
